90bf5aa80724e9b3c57f479ac88818ad96d3122e
ck <21735205+cyperdark@users.noreply.github.com>
2022-05-05 22:29:26 +0000

    update request file

--- src/utility/request.ts
+++ src/utility/request.ts
@@ -1,7 +1,17 @@
-import { RequestParams, RequestNamepsace } from "../types/request";
 import * as auth from "../utility/auth";
 import https from 'https'
 import fs from "fs";
+
+interface RequestParams {
+  method?: string,
+  headers?: { [key: string]: string },
+  data?: string,
+  params?: object | [object, ...object[]] // any | [any, ...any]
+};
+
+export interface RequestNamepsace {
+  (url: string, params: { method: string, data?: string, headers?: { [key: string]: string }, params?: object }): Promise<any>
+};
 
 const o = (obj: any) => {
   let params = '';
@@ -23,31 +33,31 @@ const o = (obj: any) => {
  * @returns {Promise<any>} The response
  */
 export const request = (url: string, { method = "GET", headers, data, params }: RequestParams = {}): Promise<any> => new Promise(async (res, rej) => {
-
-  if (url.includes('https://osu.ppy.sh/api/v2')) {
-    await auth.expired();
-    headers.Authorization = `Bearer ${auth.cache_token}`;
+  if (url.includes('https://osu.ppy.sh/api/v2')) headers = {
+    Authorization: `Bearer ${auth.cache_v2}`,
+    Accept: `application/json`,
+    'Content-Type': `application/json`,
   };
 
-  // console.log('\n', url, method, headers, data, params, '\n'); // debug too
+  console.log('\n', url, method, headers, data, o(params), '\n'); // debug too
   const req = https.request(url + (o(params) ? '?' + o(params) : ''), { method, headers }, r => {
     let data: any = '';
+    // console.log('headers', r.headers);
 
     r.on('data', (chunk: any) => data += chunk);
     r.on('end', async () => {
       if (/^application\/json/.test(r.headers['content-type']))
         try {
-          const prs = JSON.parse(data);
-          if (prs.authentication == 'basic') {
-            auth.set_expire(0);
-            auth.set_token('');
+          const parse = JSON.parse(data);
+          if (parse.authentication == 'basic' && auth.cache_v2) {
+            await auth.re_login();
 
             const again = await request(url, { method, headers, data, params });
 
             return res(again);
           };
 
-          return res(prs)
+          return res(parse)
         } catch (err) { console.log(`JSON Parse on content of type ${r.headers['content-type']} failed.\nError: ${err}\nData: ${data}`) }
 
 
@@ -61,30 +71,44 @@ export const request = (url: string, { method = "GET", headers, data, params }: 
 
 /**
  * Executes an HTTP request
- * @param {string | URL} url The url
+ * @param {string} url The url
  * @param {string} dest The file destination
  * @returns {Promise<any>} The response
  */
-export const download = (url: string | URL, dest: string, { headers, data, params }: RequestParams = {}): Promise<any> => new Promise((res, rej) => {
+export const download = (url: string, dest: string, { headers, data, params }: RequestParams = {}): Promise<any> => new Promise(async (res, rej) => {
   const file = fs.createWriteStream(dest);
 
-  const req = https.get(url + (params ? '?' + o(params) : ''), { method: 'GET', headers }, r => {
-    r.pipe(file);
-
-    file.on('finish', () => {
-      file.close();
-      res(dest);
-    });
-
-  }).on('error', (err) => {
+  file.on('error', err => {
     fs.unlinkSync(dest);
     rej(err);
+  });
+
+  file.on('finish', () => {
+    file.close();
+    res(dest);
+  });
+
+  if (url.includes('https://osu.ppy.sh/api/v2')) headers = {
+    Authorization: `Bearer ${auth.cache_v2}`,
+    Accept: `application/octet-stream`,
+    'Content-Type': `application/octet-stream`,
+  };
+
+  // console.log('\n', url, headers, data, o(params), '\n'); // debug too
+  const req = https.request(url + (params ? '?' + o(params) : ''), { method: 'GET', headers }, response => {
+    const { location } = response.headers;
+
+    if (location) {
+      const redirect = download(location, dest);
+      return res(redirect);
+    }
+
+    response.pipe(file);
   });
 
   if (data) req.write(data)
   req.end();
 });
-
 
 /**
  * Makes a namespace 
@@ -93,5 +117,5 @@ export const download = (url: string | URL, dest: string, { headers, data, param
  * @returns {(params: string, { query: string }) => Promise<any>} The function that does the reqs
  */
 export const namespace = (url: string, { query }: { query?: { [key: string]: string }, headers?: Record<string, string> } = {}): RequestNamepsace => {
-  return (path: string, params) => request(url + path, params)
+  return (path: string, params) => request(url + path, params);
 };
