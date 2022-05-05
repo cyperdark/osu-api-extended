4e14b9903bd8655a5482254c9e36a513d360ea5c
ck <21735205+cyperdark@users.noreply.github.com>
2022-05-05 22:32:49 +0000

    changes 1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-export * as v1 from "./api/v1/index";
-export * as v2 from "./api/v2/index";
+// export * as v1 from "./api/v1/index";
+// export * as v2 from "./api/v2/index";
 
-export * as mods from './utility/mods';
-export * as tools from './utility/tools';
+// export * as mods from './utility/mods';
+// export * as tools from './utility/tools';
 
 export * as auth from './utility/auth'
+
+
+export * as v2 from "./api/v2/index";
