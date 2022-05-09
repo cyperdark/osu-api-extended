7310767286eca96ce73226a57b609665e693f64b
ck <21735205+cyperdark@users.noreply.github.com>
2022-05-09 20:22:32 +0000

    tools

--- src/utility/tools/index.ts
+++ src/utility/tools/index.ts
@@ -1,7 +1,14 @@
-import country from "./routes/country";
-import accuracy from "./routes/accuracy";
+import _rank from "./routes/rank";
+import _country from "./routes/country";
+import _accuracy from "./routes/accuracy";
+import _download_difficulty from "./routes/download/difficulty";
 
-export  {
-  country,
-  accuracy,
+export const rank = _rank;
+
+export const country = _country
+
+export const accuracy = _accuracy
+
+export const download = {
+  difficulty: _download_difficulty
 };
---
+++ src/utility/tools/routes/rank.ts
@@ -0,0 +1,102 @@
+import { name as mods_name } from "../../mods";
+
+export interface types {
+  (hits: {
+    300: string,
+    100: string,
+    50: string,
+    0: string,
+    geki: string,
+    katu: string
+  }, mods: any, mode: 'osu' | 'fruits' | 'taiko' | 'mania'): string;
+};
+
+const name: types = (hits, mods, mode = 'osu'): string => {
+  const h300 = parseInt(hits[300]);
+  const h100 = parseInt(hits[100]);
+  const h50 = parseInt(hits[50]);
+  const h0 = parseInt(hits[0]);
+  const geki = parseInt(hits.geki);
+  const katu = parseInt(hits.katu);
+  
+  if (!isNaN(mods)) mods = mods_name(mods);
+  const silver = mods.toLowerCase().indexOf('hd') > -1 ? true : mods.toLowerCase().indexOf('fl') > -1 ? true : false;
+
+  let total = 0;
+  let acc = 0.0;
+
+  let r300 = 0;
+  let r50 = 0;
+
+  let rank = '';
+
+  switch (mode) {
+    case 'osu':
+      total = +h300 + +h100 + +h50 + +h0;
+      acc = total > 0 ? (+h50 * 50 + +h100 * 100 + +h300 * 300) / (total * 300) : 1;
+
+      r300 = +h300 / total;
+      r50 = +h50 / total;
+
+      if (r300 === 1) rank = silver ? 'XH' : 'X';
+      else if (r300 > 0.9 && r50 < 0.01 && h0 === 0) rank = silver ? 'SH' : 'S';
+      else if ((r300 > 0.8 && h0 === 0) || r300 > 0.9) rank = 'A';
+      else if ((r300 > 0.7 && h0 === 0) || r300 > 0.8) rank = 'B';
+      else if (r300 > 0.6) rank = 'C';
+      else rank = 'D';
+
+      break;
+
+    case 'taiko':
+      total = +h300 + +h100 + +h50 + +h0;
+      acc = total > 0 ? (+h100 * 150 + +h300 * 300) / (total * 300) : 1;
+
+      r300 = +h300 / total;
+      r50 = +h50 / total;
+
+      if (r300 === 1) rank = silver ? 'XH' : 'X';
+      else if (r300 > 0.9 && r50 < 0.01 && h0 === 0) rank = silver ? 'SH' : 'S';
+      else if ((r300 > 0.8 && h0 === 0) || r300 > 0.9) rank = 'A';
+      else if ((r300 > 0.7 && h0 === 0) || r300 > 0.8) rank = 'B';
+      else if (r300 > 0.6) rank = 'C';
+      else rank = 'D';
+
+      break;
+
+    case 'fruits':
+      total = +h300 + +h100 + +h50 + +h0 + +katu;
+      acc = total > 0 ? (+h50 + +h100 + +h300) / total : 1;
+
+      r300 = +h300 / total;
+      r50 = +h50 / total;
+
+      if (acc === 1) rank = silver ? 'XH' : 'X';
+      else if (acc > 0.98) rank = silver ? 'SH' : 'S';
+      else if (acc > 0.94) rank = 'A';
+      else if (acc > 0.9) rank = 'B';
+      else if (acc > 0.85) rank = 'C';
+      else rank = 'D';
+
+      break;
+
+    case 'mania':
+      total = +h300 + +h100 + +h50 + +h0 + +geki + +katu;
+      acc = total > 0 ? (+h50 * 50 + +h100 * 100 + +katu * 200 + (+h300 + +geki) * 300) / (total * 300) : 1;
+
+      r300 = +h300 / total;
+      r50 = +h50 / total;
+
+      if (acc === 1) rank = silver ? 'XH' : 'X';
+      else if (acc > 0.95) rank = silver ? 'SH' : 'S';
+      else if (acc > 0.9) rank = 'A';
+      else if (acc > 0.8) rank = 'B';
+      else if (acc > 0.7) rank = 'C';
+      else rank = 'D';
+
+      break;
+  };
+
+  return rank;
+};
+
+export default name;
