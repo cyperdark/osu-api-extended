400d80e71291c4cd1b198edc03aa3d31f6782211
owo <aburachenko2000@gmail.com>
2023-01-12 23:26:17 +0300

    export download request

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,6 @@ export * as auth from './utility/auth'
 export * as v1 from "./api/v1/index";
 export * as v2 from "./api/v2/index";
 
+export { download } from "./utility/request";
 
 // export * as md from './md';
