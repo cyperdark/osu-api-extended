57f60fad5b9ebea78c9db66016a9feabe611b9df
ck <21735205+cyperdark@users.noreply.github.com>
2023-04-04 19:16:22 +0300

    new tool: total objects

--- src/utility/tools/index.ts
+++ src/utility/tools/index.ts
@@ -1,6 +1,7 @@
 import _rank, { description as _rank_desc } from "./routes/rank";
 import _country, { description as _country_desc } from "./routes/country";
 import _accuracy, { description as _accuracy_desc } from "./routes/accuracy";
+import _total_objects, { description as _total_objects_desc } from "./routes/total_objects";
 import _download_difficulty, { description as _download_difficulty_desc } from "./routes/download/difficulty";
 import _pp_calculate, { description as _pp_calculate_desc } from "./routes/pp/calculate";
 
@@ -8,6 +9,7 @@ import _pp_calculate, { description as _pp_calculate_desc } from "./routes/pp/ca
 export const rank = _rank;
 export const country = _country
 export const accuracy = _accuracy
+export const total_objects = _total_objects;
 export const download = {
   difficulty: _download_difficulty
 };
@@ -20,6 +22,7 @@ export const desc: {
   rank: object,
   country: object,
   accuracy: object,
+  total_objects: object,
   download: {
     difficulty: object,
   },
@@ -30,6 +33,7 @@ export const desc: {
   rank: _rank_desc,
   country: _country_desc,
   accuracy: _accuracy_desc,
+  total_objects: _total_objects_desc,
   download: {
     difficulty: _download_difficulty_desc,
   },
