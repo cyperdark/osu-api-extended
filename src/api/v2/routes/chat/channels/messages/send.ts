7eb7495c164e382524729761ef946246a95e8434
cyperdark <aburachenko2000@gmail.com>
2023-05-22 23:54:07 +0300

    last one??

--- src/api/v2/routes/chat/channels/messages/send.ts
+++ src/api/v2/routes/chat/channels/messages/send.ts
@@ -1,18 +1,22 @@
+import { types } from '../../../../../../types/v2_chat_channels_messages_send';
+import { Description } from '../../../../../../utility/types';
+
+
 import { namespace, RequestNamepsace } from "../../../../../../utility/request";
 const request: RequestNamepsace = namespace('https://osu.ppy.sh/api/v2/');
 
 
-export const description: any = {
+export const description: Description = {
   auth: 0,
   title: __filename,
   method: 'GET',
-  description: 'Create new PM or send new message',
+  description: 'Send message to chat channel',
   params: [
     {
       type: 'number',
-      name: 'user_id',
+      name: 'channel_id',
       optional: false,
-      description: 'id of the user',
+      description: 'The channel_id of the channel to send message to',
     },
     {
       type: 'number',
@@ -27,59 +31,14 @@ export const description: any = {
       description: 'Whether the message is an action',
     },
   ],
+  return: 'response',
 };
 
-export interface types {
-  (user_id: number, message: string, is_action: true | false): Promise<response>;
-};
 
-export interface response {
-  channel: {
-    channel_id: number;
-    description: string;
-    icon: string;
-    moderated: boolean;
-    name: string;
-    type: string;
-    last_message_id: number;
-    users: number[];
-  };
-  message: {
-    message_id: number;
-    sender_id: number;
-    channel_id: number;
-    timestamp: string;
-    content: string;
-    is_action: boolean;
-    sender: {
-      avatar_url: string;
-      country_code: string;
-      default_group: string;
-      id: number;
-      is_active: boolean;
-      is_bot: boolean;
-      is_deleted: boolean;
-      is_online: boolean;
-      is_supporter: boolean;
-      last_visit: string;
-      pm_friends_only: boolean;
-      profile_colour: string;
-      username: string;
-    };
-  };
-  new_channel_id: number;
-}
-
-
-
-const name: types = async (user_id: number, message: string, is_action: true | false) => {
-  const data = await request(`chat/new`, {
+const name: types = async (channel_id, message, is_action) => {
+  const data = await request(`chat/channels/${channel_id}/messages`, {
     method: 'POST',
-    data: JSON.stringify({
-      target_id: user_id,
-      message: message,
-      is_action: is_action,
-    }),
+    data: JSON.stringify({ message, is_action }),
   });
 
   return data;
