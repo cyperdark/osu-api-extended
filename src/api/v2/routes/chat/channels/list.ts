453a339209c64fea368b38b48fb0dd34675881c6
cyperdark <aburachenko2000@gmail.com>
2023-05-22 23:05:38 +0300

    update routes

--- src/api/v2/routes/chat/channels/list.ts
+++ src/api/v2/routes/chat/channels/list.ts
@@ -1,31 +1,19 @@
+import { types } from '../../../../../types/v2_chat_channels_list';
+import { Description } from '../../../../../utility/types';
+
+
 import { namespace, RequestNamepsace } from "../../../../../utility/request";
 const request: RequestNamepsace = namespace('https://osu.ppy.sh/api/v2/');
 
 
-export const description: any = {
+export const description: Description = {
   auth: 0,
   title: __filename,
   method: 'GET',
   description: 'Return list of channels',
-  params: [
-
-  ],
+  params: [],
+  return: 'response',
 };
-
-export interface types {
-  (): Promise<response[]>;
-};
-
-export interface response {
-  channel_id: number;
-  description: string;
-  icon: string;
-  moderated: boolean;
-  name: string;
-  type: string;
-}
-
-
 
 
 const name: types = async () => {
---
+++ src/types/v2_chat_updates.ts
@@ -0,0 +1,48 @@
+export interface response {
+  presence: {
+    channel_id: number;
+    description: string;
+    icon: string;
+    moderated: boolean;
+    name: string;
+    type: string;
+    uuid?: string;
+    current_user_attributes: {
+      can_message: boolean;
+      can_message_error: string;
+      last_read_id: number;
+    };
+    last_message_id: number;
+    last_read_id?: number;
+    users: number[];
+  }[];
+  silences: [];
+  messages: [];
+}
+
+
+export interface types {
+  /**
+   * This endpoint returns new messages since the given message_id along with updated channel 'presence' data.
+   * 
+   * ## Example 
+   * 
+   * ```js
+   * const { v2, auth } = require('osu-api-extended');
+   * 
+   * const main = async () => {
+   *   await auth.login_lazer(USERNAME, USER_PASSWORD);
+   *
+   *   const v2_chat_updates = await v2.chat.updates(object);
+   *   console.log(v2_chat_updates);
+   * };
+   * 
+   * main();
+   * ```
+   * @param {number} object.channel_id Messages after the specified message_id to return
+   * @param {number} object.history_since UserSilence after the specified id to return
+   * @param {string[]} object.includes ```presence```, ```messages```, ```silences```
+   * @param {number} object.since Messages after the specified message_id to return
+  */
+  (object: { channel_id?: number, history_since?: number, includes?: ['presence, messages, silences'], since: number, }): Promise<response>;
+}
