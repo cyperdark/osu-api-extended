b5c34fdd94555162cb99ffbf5dbe054701d4ba18
ck <21735205+cyperdark@users.noreply.github.com>
2023-05-26 04:06:31 +0300

    build auth url & retrive user details from auth code

--- src/utility/auth.ts
+++ src/utility/auth.ts
@@ -1,6 +1,11 @@
-import { request } from "../utility/request";
 import readln from "readline";
 import open from "open";
+
+
+import { types as user_types } from '../types/v2_user_me_details';
+import { request } from "../utility/request";
+import { osu_mode } from './types';
+
 
 interface _login {
   access_token: string,
@@ -47,13 +52,15 @@ const save_credentials = (type: number, obj: any) => {
   };
 };
 
+
 export const re_login = async () => {
   if (credentials.type == 1) await login_lazer(credentials.username, credentials.password);
   if (credentials.type == 2) await login(credentials.clientId, credentials.clientSecret);
-  if (credentials.type == 3) await authorize(credentials.clientId, credentials.clientSecret, credentials.redirect_uri);
+  if (credentials.type == 3) await authorize_cli(credentials.clientId, credentials.clientSecret, credentials.redirect_uri);
 
   return true;
 };
+
 
 export const login_lazer = async (username: string, password: string): Promise<_login> => {
   if (!isInitial()) save_credentials(1, { username, password });
@@ -79,6 +86,7 @@ export const login_lazer = async (username: string, password: string): Promise<_
   return { access_token, expires_in };
 };
 
+
 export const login = async (clientId: number, clientSecret: string): Promise<_login> => {
   if (!isInitial()) save_credentials(2, { clientId, clientSecret });
 
@@ -102,13 +110,14 @@ export const login = async (clientId: number, clientSecret: string): Promise<_lo
   return { access_token, expires_in };
 };
 
-export const authorize = async (clientId: number, clientSecret: string, redirect_uri: string, scope?: string, state?: string): Promise<_login> => {
-  if (!isInitial()) save_credentials(3, { clientId, clientSecret, redirect_uri });
+
+export const authorize_cli = async (clientId: number, clientSecret: string, redirectUri: string, scope?: string, state?: string): Promise<_login> => {
+  if (!isInitial()) save_credentials(3, { clientId, clientSecret, redirect_uri: redirectUri });
 
   const cl = readln.createInterface(process.stdin, process.stdout);
   const question = (q: string) => new Promise((res, rej) => cl.question(q + ': ', (answer: string) => res(answer)));
 
-  await open(`https://osu.ppy.sh/oauth/authorize?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=code&scope=friends.read%20identify%20public`);
+  await open(`https://osu.ppy.sh/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=friends.read%20identify%20public`);
 
   const code = await question('Paste code here');
   const { access_token, expires_in } = await request('https://osu.ppy.sh/oauth/token', {
@@ -121,7 +130,7 @@ export const authorize = async (clientId: number, clientSecret: string, redirect
       grant_type: 'authorization_code',
       client_id: clientId,
       client_secret: clientSecret,
-      redirect_uri,
+      redirect_uri: redirectUri,
       code,
     })
   });
@@ -129,4 +138,43 @@ export const authorize = async (clientId: number, clientSecret: string, redirect
   cache_v2 = access_token;
 
   return { access_token, expires_in };
+};
+
+
+export const build_url = (clientId: number, redirectUri: string, scope: ['chat.write' | 'delegate' | 'forum.write' | 'friends.read' | 'identify' | 'public'], state?: string): string => {
+  const url = new URL('https://osu.ppy.sh/oauth/authorize');
+  const params: any = {
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    response_type: 'code',
+    scope: scope.join(' '),
+    state: state,
+  };
+
+  Object.keys(params)
+    .forEach(key => url.searchParams.append(key, params[key]));
+
+  return url.href;
+};
+
+
+export const authorize = async (code: string, gamemode: osu_mode, clientId: number, clientSecret: string, redirectUri: string): Promise<any> => {
+  const { access_token } = await request('https://osu.ppy.sh/oauth/token', {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      code,
+    })
+  });
+
+
+  const user = await request(`https://osu.ppy.sh/api/v2/me/${gamemode}`, { params: { v2: access_token } });
+  return user;
 };
