c87e343907e41941cc40a2b0d29348f3a85b516d
ck <services.useranem@gmail.com>
2023-09-24 09:14:34 +0300

    leftovers

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-// export * as v1 from "./api/v1/index";
-// export * as v2 from "./api/v2/index";
-
 export * as country from './tools/country';
 export * as mods from './tools/mods';
 
 export * as auth from './utility/auth';
 
 
-// export * as v1 from "./api/v1";
 export * as v2 from "./api/v2/index";
