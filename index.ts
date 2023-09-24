90cd2e72d92015801a2e3ced7a0151c9a5565ea3
ck <services.useranem@gmail.com>
2023-09-24 09:07:37 +0300

    export methods 1/?

---
+++ index.ts
@@ -0,0 +1,11 @@
+// export * as v1 from "./api/v1/index";
+// export * as v2 from "./api/v2/index";
+
+export * as country from './tools/country';
+export * as mods from './tools/mods';
+
+export * as auth from './utility/auth';
+
+
+// export * as v1 from "./api/v1";
+export * as v2 from "./api/v2/index";
