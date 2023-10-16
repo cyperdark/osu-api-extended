50f141b5d40bd9080325d49c794243654c6fa751
Yoru <m.f.fetvaci@hotmail.com>
2023-10-16 15:28:58 +0300

    export methodsEnum

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 // export * as v1 from "./api/v1/index";
 // export * as v2 from "./api/v2/index";
 
-export * as mods from './utility/mods';
-export * as tools from './utility/tools/index';
+export * as mods from "./utility/mods";
+export * as tools from "./utility/tools/index";
 
-export * as auth from './utility/auth'
-
+export * as auth from "./utility/auth";
 
 export * as v1 from "./api/v1/index";
 export * as v2 from "./api/v2/index";
 
 export { download } from "./utility/request";
+export { MethodsEnum } from "./utility/types";
 
 // export * as md from './md';
