90b7d08a3b70b9849ef8d5159a3fc3ba099bf67c
ck <21735205+cyperdark@users.noreply.github.com>
2023-12-11 07:49:44 +0300

    reworked auth methods

--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,50 @@ export type ranking_types = 'charts' | 'country' | 'performance' | 'score';
 
 export type country_names = "United States" | "Russian Federation" | "Germany" | "Poland" | "France" | "Japan" | "Canada" | "Brazil" | "United Kingdom" | "Taiwan" | "South Korea" | "China" | "Australia" | "Indonesia" | "Ukraine" | "Philippines" | "Chile" | "Finland" | "Argentina" | "Netherlands" | "Sweden" | "Singapore" | "Mexico" | "Malaysia" | "Spain" | "Italy" | "Hong Kong" | "Thailand" | "Vietnam" | "Norway" | "Czech Republic" | "Turkey" | "Belarus" | "Austria" | "Belgium" | "Portugal" | "Romania" | "Hungary" | "Denmark" | "Lithuania" | "Kazakhstan" | "New Zealand" | "Peru" | "Switzerland" | "Colombia" | "Israel" | "Estonia" | "Bulgaria" | "Slovakia" | "Greece" | "Latvia" | "Venezuela" | "Serbia" | "Ireland" | "Croatia" | "Saudi Arabia" | "Uruguay" | "South Africa" | "United Arab Emirates" | "Slovenia" | "India" | "Ecuador" | "Morocco" | "Costa Rica" | "Moldova" | "Dominican Republic" | "Brunei" | "Egypt" | "Reunion" | "Macau" | "Tunisia" | "Panama" | "Algeria" | "Mongolia" | "Paraguay" | "Kuwait" | "Puerto Rico" | "Georgia" | "Bolivia" | "El Salvador" | "Qatar" | "Guatemala" | "Luxembourg" | "Uzbekistan" | "Kyrgyzstan" | "North Macedonia" | "Cambodia" | "Bosnia and Herzegovina" | "Iceland" | "Jordan" | "Pakistan" | "Trinidad and Tobago" | "Cyprus" | "Honduras" | "Nicaragua" | "Bahrain" | "Maldives" | "Bangladesh" | "French Polynesia" | "Lebanon" | "Nepal" | "Iraq" | "Guam" | "Azerbaijan" | "Albania" | "Malta" | "Myanmar" | "New Caledonia" | "Oman" | "Iran, Islamic Republic of" | "Armenia" | "Guadeloupe" | "Martinique" | "Jamaica" | "Lao People &#039;s Democratic Republic" | "Sri Lanka" | "Palestinian Territory Occupied" | "Mauritius" | "Syrian Arab Republic" | "Montenegro" | "Faroe Islands" | "Jersey" | "French Guiana" | "Northern Mariana Islands" | "Isle of Man" | "Suriname" | "Belize" | "Barbados" | "Aruba" | "Libya" | "Aland Islands" | "Guernsey" | "Madagascar" | "Sudan" | "Liechtenstein" | "Bahamas" | "Kenya" | "Greenland" | "Bermuda" | "Europe" | "Cote D &#039; Ivoire" | "Guyana" | "Gibraltar" | "Virgin Islands, U.S." | "Andorra" | "Saint Lucia" | "Tajikistan" | "Antigua and Barbuda" | "Senegal" | "Saint Pierre and Miquelon" | "Cook Islands" | "Antarctica" | "Central African Republic" | "Liberia" | "Eritrea";
 export type country_codes = "US" | "RU" | "DE" | "PL" | "FR" | "JP" | "CA" | "BR" | "GB" | "TW" | "KR" | "CN" | "AU" | "ID" | "UA" | "PH" | "CL" | "FI" | "AR" | "NL" | "SE" | "SG" | "MX" | "MY" | "ES" | "IT" | "HK" | "TH" | "VN" | "NO" | "CZ" | "TR" | "BY" | "AT" | "BE" | "PT" | "RO" | "HU" | "DK" | "LT" | "KZ" | "NZ" | "PE" | "CH" | "CO" | "IL" | "EE" | "BG" | "SK" | "GR" | "LV" | "VE" | "RS" | "IE" | "HR" | "SA" | "UY" | "ZA" | "AE" | "SI" | "IN" | "EC" | "MA" | "CR" | "MD" | "DO" | "BN" | "EG" | "RE" | "MO" | "TN" | "PA" | "DZ" | "MN" | "PY" | "KW" | "PR" | "GE" | "BO" | "SV" | "QA" | "GT" | "LU" | "UZ" | "KG" | "MK" | "KH" | "BA" | "IS" | "JO" | "PK" | "TT" | "CY" | "HN" | "NI" | "BH" | "MV" | "BD" | "PF" | "LB" | "NP" | "IQ" | "GU" | "AZ" | "AL" | "MT" | "MM" | "NC" | "OM" | "IR" | "AM" | "GP" | "MQ" | "JM" | "LA" | "LK" | "PS" | "MU" | "SY" | "ME" | "FO" | "JE" | "GF" | "MP" | "IM" | "SR" | "BZ" | "BB" | "AW" | "LY" | "AX" | "GG" | "MG" | "SD" | "LI" | "BS" | "KE" | "GL" | "BM" | "EU" | "CI" | "GY" | "GI" | "VI" | "AD" | "LC" | "TJ" | "AG" | "SN" | "PM" | "CK" | "AQ" | "CF" | "LR" | "ER";
+
+
+export type auth_scopes = (
+  'chat.read' |
+  'chat.write' |
+  'chat.write_manage' |
+  'delegate' |
+  'forum.write' |
+  'friends.read' |
+  'identify' |
+  'public'
+)[];
+
+
+export type auth_params = ({
+  method: 'stable v2';
+
+  client_id: number | string;
+  client_secret: string;
+
+  scopes: auth_scopes;
+} | {
+  method: 'stable v1';
+
+  api_key: string;
+} | {
+  method: 'lazer'
+
+  login: string;
+  password: string;
+} | {
+  method: 'cli';
+
+  client_id: number | string;
+  client_secret: string;
+
+  redirect_url: string;
+  state: string;
+
+  scopes: auth_scopes;
+});
+
+
+export type auth_response = {
+  access_token: string;
+  expires_in: number;
+};
--- utility/auth.ts
+++ utility/auth.ts
@@ -1,120 +1,109 @@
-// PACKAGES
-// import { execSync } from 'child_process';
-// import readln from "readline";
-
-
-// IMPORTS
+import { execSync } from 'child_process';
 import { request } from "./request";
-// import { gamemode_names } from './types';
+import readln from "readline";
 
 
-export type auth_scopes = [
-  'chat.read' |
-  'chat.write' |
-  'chat.write_manage' |
-  'delegate' |
-  'forum.write' |
-  'friends.read' |
-  'identify' |
-  'public'
-];
+import { auth_params, auth_response, auth_scopes, gamemode_names } from '../types/index';
+import { UserDetails } from '../types/user_details';
 
-export type auth_login = {
-  access_token: string,
-  expires_in: number
-}
 
 const credentials: {
-  type: number,
-  username: string,
-  password: string,
-  clientId: number,
-  clientSecret: string,
-  redirect_uri: string,
-  scopes: auth_scopes,
+  method: any;
+
+  api_key: string;
+
+  client_id: number | string;
+  client_secret: string;
+
+  login: string;
+  password: string;
+
+  redirect_url: string;
+  state: string;
+
+  scopes: auth_scopes;
 } = {
-  type: 0,
-  username: '',
+  method: '' as any,
+
+  api_key: '',
+
+  client_id: '',
+  client_secret: '',
+
+  login: '',
   password: '',
-  clientId: 0,
-  clientSecret: '',
-  redirect_uri: '',
+
+  redirect_url: '',
+  state: '',
+
   scopes: ['public'],
 };
 
-
-export let cache_v1: string = '';
-export let cache_v2: string = '';
-
-
-export const set_v1 = (v: string) => cache_v1 = v;
-export const set_v2 = (v: string) => cache_v2 = v;
-
-const isInitial = () => cache_v2 != '';
-
-const store_credentials = (type: number, obj: {
-  username?: string, password?: string, clientId?: number, clientSecret?: string, scopes?: auth_scopes, redirect_uri?: string
-}) => {
-  if (type == 1) {
-    credentials.type = 1;
-    credentials.username = obj.username;
-    credentials.password = obj.password;
-  };
-
-  if (type == 2) {
-    credentials.type = 2;
-    credentials.clientId = obj.clientId;
-    credentials.clientSecret = obj.clientSecret;
-    credentials.scopes = obj.scopes;
-  };
-
-  if (type == 3) {
-    credentials.type = 3;
-    credentials.clientId = obj.clientId;
-    credentials.clientSecret = obj.clientSecret;
-    credentials.redirect_uri = obj.redirect_uri;
-    credentials.scopes = obj.scopes;
-  };
+export const cache_tokens = {
+  v1: '',
+  v2: '',
 };
 
 
-export const update_cache_token = async () => {
-  // if (credentials.type == 1) await lazer_user(credentials.username, credentials.password);
-  if (credentials.type == 2) await client_login(credentials.clientId, credentials.clientSecret, credentials.scopes);
-  // if (credentials.type == 3) await cli(credentials.clientId, credentials.clientSecret, credentials.redirect_uri, credentials.scope);
 
-  return true;
+export const login = async (params: auth_params) => {
+  credentials.method = params.method;
+
+
+  if (params.method == 'stable v1') {
+    credentials.api_key = params.api_key;
+
+    cache_tokens.v1 = params.api_key;
+    return cache_tokens.v1;
+  };
+
+
+  if (params.method == 'stable v2') {
+    credentials.client_id = params.client_id;
+    credentials.client_secret = params.client_secret;
+    if (params.scopes) credentials.scopes = params.scopes;
+
+    const authorize = await client_login(params.client_id, params.client_secret, params.scopes || ['public']);
+    return authorize;
+  };
+
+
+  if (params.method == 'lazer') {
+    credentials.login = params.login;
+    credentials.password = params.password;
+
+    const authorize = await lazer_login(params.login, params.password);
+    return authorize;
+  };
+
+
+  if (params.method == 'cli') {
+    credentials.client_id = params.client_id;
+    credentials.client_secret = params.client_secret;
+    if (params.scopes) credentials.scopes = params.scopes;
+
+    credentials.redirect_url = params.redirect_url;
+    credentials.state = params.state;
+
+    const authorize = await authorize_cli(params.client_id, params.client_secret, params.redirect_url, params.scopes || ['public'], params.state);
+    return authorize;
+  };
+
+
+  throw new Error('Unknown login method');
 };
 
-/**
- * # Authorization via osu client
- * - clientId - osu Client ID
- * - clientSecret - osu Client Secret
- * - _**(optional)**_ scopes - list of scopes
- *   - chat.read - Allows read chat messages on a user's behalf.
- *   - chat.write - Allows sending chat messages on a user's behalf.
- *   - chat.write_manage - Allows joining and leaving chat channels on a user's behalf.
- *   - delegate - Allows acting as the owner of a client; only available for Client Credentials Grant.
- *   - forum.write - Allows creating and editing forum posts on a user's behalf.
- *   - friends.read - Allows reading of the user's friend list.
- *   - identify - Allows reading of the public profile of the user (/me).
- *   - public - Allows reading of publicly available data on behalf of the user.
- */
-export const client_login = async (clientId: number, clientSecret: string, scopes?: auth_scopes): Promise<auth_login> => {
-  if (scopes == null)
-    throw new Error('Specify scopes');
 
-  if (scopes != null && !Array.isArray(scopes))
-    throw new Error('Scopes must be an Array');
+export const set_v2 = (token: string) => cache_tokens.v2 = token;
+
+export const refresh_token = async () => {
+  const refresh = await login(credentials);
+  return refresh;
+};
 
 
-  // add public scopes
-  if (scopes == null)
-    scopes = ['public'];
 
-
-  if (!isInitial()) store_credentials(2, { clientId, clientSecret, scopes });
-
+const client_login = async (client_id: number | string, client_secret: string, scopes: auth_scopes): Promise<auth_response> => {
   const { access_token, expires_in } = await request('https://osu.ppy.sh/oauth/token', {
     method: 'POST',
     headers: {
@@ -123,14 +112,120 @@ export const client_login = async (clientId: number, clientSecret: string, scope
     },
     data: JSON.stringify({
       grant_type: 'client_credentials',
-      client_id: clientId,
-      client_secret: clientSecret,
+      client_id: client_id,
+      client_secret: client_secret,
       scope: scopes.join(' '),
       code: 'code',
     })
   });
 
 
-  cache_v2 = access_token;
+  cache_tokens.v2 = access_token;
   return { access_token, expires_in };
+};
+
+
+const lazer_login = async (login: string, password: string) => {
+  const { access_token, expires_in } = await request('https://osu.ppy.sh/oauth/token', {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      username: login,
+      password: password,
+      grant_type: "password",
+      client_id: 5,
+      client_secret: 'FGc9GAtyHzeQDshWP5Ah7dega8hJACAJpQtw6OXk',
+      scope: "*"
+    })
+  });
+
+
+  cache_tokens.v2 = access_token;
+  return { access_token, expires_in };
+};
+
+
+const authorize_cli = async (client_id: number | string, client_secret: string, redirect_uri: string, scopes: auth_scopes, state?: string): Promise<auth_response> => {
+  const cl = readln.createInterface(process.stdin, process.stdout);
+  const question = (q: string) => new Promise((res, rej) => cl.question(q + ': ', (answer: string) => res(answer)));
+
+
+  const url = build_url({ client_id, redirect_uri, scopes, state });
+  const start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
+  execSync(start + ' ' + url.replace(/&/g, '^&'));
+
+
+  const code = await question('Paste code here');
+  const { access_token, expires_in } = await request('https://osu.ppy.sh/oauth/token', {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      grant_type: 'authorization_code',
+      client_id: client_id,
+      client_secret: client_secret,
+      redirect_uri: redirect_uri,
+      code,
+    })
+  });
+
+
+  cache_tokens.v2 = access_token;
+  return { access_token, expires_in };
+};
+
+
+export const build_url = ({ client_id, redirect_uri, scopes, state }: {
+  client_id: number | string,
+  redirect_uri: string,
+  scopes: auth_scopes,
+  state?: string
+}): string => {
+  const url = new URL('https://osu.ppy.sh/oauth/authorize');
+  const params: any = {
+    client_id: client_id,
+    redirect_uri: redirect_uri,
+    response_type: 'code',
+    scope: scopes.join(' '),
+    state: state,
+  };
+
+
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+  return url.href;
+};
+
+
+export const authorize = async ({ code, mode, client_id, client_secret, redirect_url }: {
+  code: string;
+  mode: gamemode_names;
+
+  client_id: number | string;
+  client_secret: string;
+
+  redirect_url: string;
+}): Promise<UserDetails> => {
+  const { access_token } = await request('https://osu.ppy.sh/oauth/token', {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      grant_type: 'authorization_code',
+      client_id: client_id,
+      client_secret: client_secret,
+      redirect_uri: redirect_url,
+      code,
+    })
+  });
+
+
+  const user = await request(`https://osu.ppy.sh/api/v2/me/${mode}`, { params: { v2: access_token } });
+  return user;
 };
