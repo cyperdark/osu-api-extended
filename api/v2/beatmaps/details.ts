efe9565f4f156919ba7f7abe267e9e3c2df80001
ck <21735205+cyperdark@users.noreply.github.com>
2023-12-16 12:29:53 +0300

    [endpoint] Beatmap id/set details

---
+++ api/v2/beatmaps/details.ts
@@ -0,0 +1,51 @@
+import { BeamapsDetailsDifficulty } from "../../../types/v2/beamaps_details_difficulty";
+import { BeamapsDetailsSet } from "../../../types/v2/beamaps_details_set";
+import { request } from "../../../utility/request";
+
+
+type params = ({
+  type: 'difficulty';
+  id: number;
+} | {
+  type: 'set';
+  id: number;
+});
+
+
+type Response<T extends params['type']> =
+  T extends 'difficulty' ? {
+    type: 'difficulty';
+    result: BeamapsDetailsDifficulty
+  } : T extends 'set' ? {
+    type: 'set';
+    result: BeamapsDetailsSet
+  } : never;
+
+
+const name = async <T extends params>(obj: T): Promise<Response<T['type']>> => {
+  const params: any = {};
+  let url = 'https://osu.ppy.sh/api/v2';
+  let method = 'GET';
+
+
+  switch (obj.type) {
+    case 'difficulty':
+      url += `/beatmaps/${obj.id}`;
+      break;
+
+    case 'set':
+      url += `/beatmapsets/${obj.id}`;
+      break;
+  };
+
+
+  const data = await request(url, {
+    method: method,
+    params: params,
+  });
+
+  return { type: obj.type, result: data } as Response<T['type']>;
+};
+
+
+export default name;
