0aad0e155cbe08268d65bed00968b3bd80e62e68
ck <21735205+cyperdark@users.noreply.github.com>
2023-12-17 11:47:41 +0300

    fix types

--- api/v2/forums/topic/details.ts
+++ api/v2/forums/topic/details.ts
@@ -3,12 +3,12 @@ import { ForumPostDetails } from '../../../../types/forums_topic_details';
 
 
 // FIXME: add query params
-const name = async (topic_id: number, obj: {
-  sort: 'id_asc' | 'id_desc',
-  limit: number,
-  start: string,
-  end: string,
-  cursor_string: string,
+const name = async (topic_id: number, obj?: {
+  sort?: 'id_asc' | 'id_desc',
+  limit?: number,
+  start?: string,
+  end?: string,
+  cursor_string?: string,
 }): Promise<ForumPostDetails> => {
   const data = await request(`https://osu.ppy.sh/api/v2/forums/topics/${topic_id}`, {
     method: 'GET',
