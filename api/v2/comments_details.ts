a8b33e541e6a6b54812c74cc8465f1764ddcd4a3
ck <21735205+cyperdark@users.noreply.github.com>
2024-02-24 21:16:27 +0300

    more 6/?

---
+++ api/v2/comments_details.ts
@@ -0,0 +1,17 @@
+import { IDefaultParams } from "../../types";
+import { request } from "../../utility/request";
+
+
+
+const name = async (comment_id: string, addons: IDefaultParams) => {
+  const data = await request(`https://osu.ppy.sh/api/v2/comments/${comment_id}`, {
+    method: 'GET',
+    addons,
+  });
+
+
+  return data;
+};
+
+
+export default name;
---
+++ api/v2/forum_topic_details.ts
@@ -0,0 +1,40 @@
+import { request } from "../../utility/request";
+import { ForumPostDetails } from '../../types/forums_topic_details';
+import { IDefaultParams } from "../../types";
+
+
+// FIXME: add query params
+const name = async (params: {
+  topic_id: number
+
+  start_id?: string,
+  end_id?: string,
+
+  limit?: number,
+  sort?: 'id_asc' | 'id_desc',
+
+  cursor_string?: string,
+}, addons: IDefaultParams): Promise<ForumPostDetails> => {
+  const object = {
+    start: params.start_id,
+    end: params.end_id,
+
+    sort: params.sort,
+    limit: params.limit,
+
+    cursor_string: params.cursor_string,
+  };
+
+
+  const data = await request(`https://osu.ppy.sh/api/v2/forums/topics/${params.topic_id}`, {
+    method: 'GET',
+    params: object,
+    addons,
+  });
+
+
+  return data;
+};
+
+
+export default name;
