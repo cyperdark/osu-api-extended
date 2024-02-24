e7a53ba10c130fa3f2dcf901965cad4bf5182fd8
ck <21735205+cyperdark@users.noreply.github.com>
2024-02-24 19:56:59 +0300

    more 3/?

---
+++ api/v2/beatmaps_discussions_posts.ts
@@ -0,0 +1,39 @@
+import { IDefaultParams } from "../../types";
+import { BeatmapsDiscussionsListResponse } from "../../types/v2/beatmaps_discussions_list";
+import { request } from "../../utility/request";
+
+
+
+const name = async (params: {
+  discussion_id: number;
+  sort: 'id_desc' | 'id_asc';
+  types: ('first' | 'reply' | 'system')[];
+  user: number;
+
+  limit: number;
+  cursor_string?: string;
+}, addons: IDefaultParams): Promise<BeatmapsDiscussionsListResponse> => {
+  if (params.discussion_id) {
+    // @ts-ignore
+    params.beatmapset_discussion_id = params.discussion_id;
+    delete params.discussion_id;
+  };
+
+  if (Array.isArray(params.types)) {
+    // @ts-ignore
+    params['types[]'] = params.types;
+    delete params.types;
+  };
+
+
+  const data = await request(`https://osu.ppy.sh/api/v2/beatmapsets/discussions/posts`, {
+    method: 'GET',
+    params,
+    addons
+  });
+
+  return data;
+};
+
+
+export default name;
---
+++ api/v2/beatmaps_discussions_votes.ts
@@ -0,0 +1,35 @@
+import { IDefaultParams } from "../../types";
+import { BeatmapsDiscussionsListResponse } from "../../types/v2/beatmaps_discussions_list";
+import { request } from "../../utility/request";
+
+
+
+const name = async (params: {
+  discussion_id: number;
+  sort: 'id_desc' | 'id_asc';
+  score: '1' | '-1';
+
+  user: number;
+  receiver: number;
+
+  limit: number;
+  cursor_string?: string;
+}, addons: IDefaultParams): Promise<BeatmapsDiscussionsListResponse> => {
+  if (params.discussion_id) {
+    // @ts-ignore
+    params.beatmapset_discussion_id = params.discussion_id;
+    delete params.discussion_id;
+  };
+
+
+  const data = await request(`https://osu.ppy.sh/api/v2/beatmapsets/discussions/votes`, {
+    method: 'GET',
+    params,
+    addons
+  });
+
+  return data;
+};
+
+
+export default name;
