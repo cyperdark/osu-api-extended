0863482c510578cb23051b0b73347ec19e9a23f6
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 16:08:34 +0300

    chat_channels_list - errors handling & types

--- api/v2/chat_channels_list.ts
+++ api/v2/chat_channels_list.ts
@@ -1,14 +1,14 @@
+import { IError } from "../../types";
+import { chatChannelsListResponse } from "../../types/v2/chat_channels_list";
 import { request } from "../../utility/request";
 
 
 
-const name = async () => {
+export const chat_channels_list = async (): Promise<chatChannelsListResponse | IError> => {
   const data = await request(`https://osu.ppy.sh/api/v2/chat/channels`, {
     method: 'GET',
   });
 
+
   return data;
 };
-
-
-export default name;
