0b2f9b9f57a499f349dd86f800395519e3e577a8
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 18:12:18 +0300

    matches_list - errors handling & types

--- api/v2/matches_list.ts
+++ api/v2/matches_list.ts
@@ -1,16 +1,17 @@
-import { IDefaultParams } from "../../types";
+import { IDefaultParams, IError } from "../../types";
+import { MatchesListResponse } from "../../types/v2/matches_list";
 import { request } from "../../utility/request";
 
 
 
-const name = async (params: {
+export const matches_list = async (params: {
   limit: number;
   sort: 'id_desc' | 'id_asc',
   /**
    * Use cursor.match_id
    */
   after_id: number;
-}, addons?: IDefaultParams) => {
+}, addons?: IDefaultParams): Promise<MatchesListResponse | IError> => {
   const object = {
     limit: params.limit,
     sort: params.sort,
@@ -27,6 +28,3 @@ const name = async (params: {
 
   return data;
 };
-
-
-export default name;
