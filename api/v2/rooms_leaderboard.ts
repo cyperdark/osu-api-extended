5c64e121c2ee8b46637a3d44b17d29c461ea7c27
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 18:55:58 +0300

    rooms_leaderboard - errors handling & types

--- api/v2/rooms_leaderboard.ts
+++ api/v2/rooms_leaderboard.ts
@@ -1,20 +1,24 @@
-import { IDefaultParams } from "../../types";
+import { IDefaultParams, IError } from "../../types";
+import { RoomsLeaderboardResponse } from "../../types/v2/rooms_leaderboard";
 import { request } from "../../utility/request";
 
 
 
-const name = async (params: {
+export const rooms_leaderboard = async (params: {
   id: number;
   limit?: number;
-}, addons?: IDefaultParams) => {
+}, addons?: IDefaultParams): Promise<RoomsLeaderboardResponse | IError> => {
+  if (params.id == null) {
+    return { error: new Error(`Specify room id`) };
+  };
+
+
   const data = await request(`https://osu.ppy.sh/api/v2/rooms/${params.id}/leaderboard`, {
     method: 'GET',
-    params,
+    params: { limit: params.limit },
     addons,
   });
 
+
   return data;
 };
-
-
-export default name;
