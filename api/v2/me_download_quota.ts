a750ff1de4fd0e788aa68df41071c37fc4dbf719
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 18:20:24 +0300

    me_download_quota - errors handling & types

--- api/v2/me_download_quota.ts
+++ api/v2/me_download_quota.ts
@@ -1,14 +1,19 @@
+import { credentials } from "../../utility/auth";
+import { IError } from "../../types";
+import { MedownloadquotaResponse } from "../../types/v2/me_download_quota";
 import { request } from "../../utility/request";
 
 
 
-const name = async () => {
+export const me_download_quota = async (): Promise<MedownloadquotaResponse | IError> => {
+  if (credentials.method != 'lazer') {
+    return { error: new Error(`Login via lazer to use this endpoint`) };
+  };
+
+
   const data = await request(`https://osu.ppy.sh/api/v2/me/download-quota-check`, {
     method: 'GET',
   });
 
   return data;
 };
-
-
-export default name;
---
+++ types/v2/me_download_quota.ts
@@ -0,0 +1,3 @@
+export interface MedownloadquotaResponse {
+  quota_used: number
+}
