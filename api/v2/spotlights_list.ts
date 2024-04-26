a829055380caef18a24afe1846fa587f39013193
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 19:42:45 +0300

    spotlights_list - errors handling & types

--- api/v2/spotlights_list.ts
+++ api/v2/spotlights_list.ts
@@ -1,16 +1,15 @@
-import { IDefaultParams } from "../../types";
+import { IDefaultParams, IError } from "../../types";
+import { SpotlightsListResponse } from "../../types/v2/spotlights_list";
 import { request } from "../../utility/request";
 
 
 
-const name = async (addons?: IDefaultParams) => {
+export const spotlights_list = async (addons?: IDefaultParams): Promise<SpotlightsListResponse | IError> => {
   const data = await request(`https://osu.ppy.sh/api/v2/spotlights`, {
     method: 'GET',
     addons,
   });
 
+
   return data.spotlights;
 };
-
-
-export default name;
