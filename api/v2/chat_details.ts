132e7d41be7f187037d7f307d2902fc2199f6a40
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 16:18:49 +0300

    chat_details - errors handling & types

--- api/v2/chat_details.ts
+++ api/v2/chat_details.ts
@@ -1,16 +1,20 @@
-import { IDefaultParams } from "../../types";
+import { IDefaultParams, IError } from "../../types";
+import { ChatDetailsResponse } from "../../types/v2/chat_details";
 import { request } from "../../utility/request";
 
 
 
-const name = async (channel_id: number, addons?: IDefaultParams) => {
+export const chat_details = async (channel_id: number, addons?: IDefaultParams): Promise<ChatDetailsResponse | IError> => {
+  if (channel_id == null) {
+    return { error: new Error(`Specify channel id`) };
+  };
+
+
   const data = await request(`https://osu.ppy.sh/api/v2/chat/channels/${channel_id}`, {
     method: 'GET',
     addons,
   });
 
+
   return data;
 };
-
-
-export default name;
