04daec1a9e58beecec72982589bf3acfeeda2800
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 20:06:21 +0300

    move v1

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-// export * as v1 from "./api/v1/index";
-// export * as v2 from "./api/v2/index";
-
 export * as mods from './utility/mods';
 export * as tools from './utility/tools/index';
-
-export * as auth from './utility/auth'
-
-
-export * as v1 from "./api/v1/index";
-export * as v2 from "./api/v2/index";
-
-export { download } from "./utility/request";
-
-// export * as md from './md';
