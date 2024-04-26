0854303c4199e1970d3363b025af9304af5c7241
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 17:43:26 +0300

    matches_details - errors handling & types

--- api/v2/matches_details.ts
+++ api/v2/matches_details.ts
@@ -1,17 +1,20 @@
-import { IDefaultParams } from "../../types";
+import { IDefaultParams, IError } from "../../types";
+import { MatchesDetailsResponse } from "../../types/v2/matches_detaIls";
 import { request } from "../../utility/request";
 
 
 
-const name = async (match_id: number, addons?: IDefaultParams) => {
+export const matches_details = async (match_id: number, addons?: IDefaultParams): Promise<MatchesDetailsResponse | IError> => {
+  if (match_id == null) {
+    return { error: new Error(`Specify match id`) };
+  };
+
+
   const data = await request(`https://osu.ppy.sh/api/v2/matches/${match_id}`, {
     method: 'GET',
     addons,
   });
 
-  
+
   return data;
 };
-
-
-export default name;
