5e63d976d308fa23b7fcfcd95a36d3046302b2cd
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-26 18:24:54 +0300

    news_details - errors handling & types

--- api/v2/news_details.ts
+++ api/v2/news_details.ts
@@ -1,20 +1,24 @@
-import { IDefaultParams } from "../../types";
+import { IDefaultParams, IError } from "../../types";
+import { NewsDetailsResponse } from "../../types/v2/news_details";
 import { request } from "../../utility/request";
 
 
 
-const name = async (params: {
+export const news_details = async (params: {
   news_id: string;
   key?: 'id' | null;
-}, addons?: IDefaultParams) => {
+}, addons?: IDefaultParams): Promise<NewsDetailsResponse | IError> => {
+  if (params.news_id == null) {
+    return { error: new Error(`Specify news id`) };
+  };
+
+
   const data = await request(`https://osu.ppy.sh/api/v2/news/${params.news_id}`, {
     method: 'GET',
     params: { key: params?.key },
     addons,
   });
 
+
   return data;
 };
-
-
-export default name;
