50d35488e183a6abcc25c37d7d90e00e7b1fa78a
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-27 12:22:48 +0300

    accuracy improve

--- tools/accuracy.ts
+++ tools/accuracy.ts
@@ -1,33 +1,80 @@
-export interface types {
-  (hits: {
-    300: string,
-    100: string,
-    50: string,
-    0: string,
-    geki: string,
-    katu: string
-  }, mode: 'osu' | 'fruits' | 'taiko' | 'mania'): number;
+import { IError } from "../types";
+import { GamemodeEnum } from "../types/enums";
+
+
+type Hits = {
+  300?: any;
+  100?: any;
+  50?: any;
+  0?: any;
+  geki?: any;
+  katu?: any;
+
+  perfect?: any;
+  great?: any;
+  good?: any;
+  ok?: any;
+  meh?: any;
+  miss?: any;
 };
 
+type Responnse = {
+  accuracy: number;
+  fc_accuracy: number;
+} | IError;
 
-const name: types = (hits, mode = 'osu') => {
-  const h300 = parseInt(hits[300]);
-  const h100 = parseInt(hits[100]);
-  const h50 = parseInt(hits[50]);
-  const h0 = parseInt(hits[0]);
-  const geki = parseInt(hits.geki);
-  const katu = parseInt(hits.katu);
 
-  let acc = 0.0;
 
-  switch (mode) {
-    case 'osu': acc = (100.0 * (6 * h300 + 2 * h100 + h50)) / (6 * (h50 + h100 + h300 + h0)); break;
-    case 'taiko': acc = (100.0 * (2 * h300 + h100)) / (2 * (h300 + h100 + h0)); break;
-    case 'fruits': acc = (100.0 * (h300 + h100 + h50)) / (h300 + h100 + h50 + katu + h0); break;
-    case 'mania': acc = (100.0 * (6 * geki + 6 * h300 + 4 * katu + 2 * h100 + h50)) / (6 * (h50 + h100 + h300 + h0 + geki + katu)); break;
+export const calculate_accuracy = (hits: Hits, mode: GamemodeEnum): Responnse => {
+  if (Object.keys(hits).length == 0) {
+    return { error: new Error('Provide hits (300, 100, 50, etc)') };
   };
 
-  return parseFloat(acc.toFixed(2));
-};
 
-export default name;
+  const geki = parseInt(hits?.geki || hits.perfect || '0');
+  const h300 = parseInt(hits?.[300] || hits.great || '0');
+  const katu = parseInt(hits?.katu || hits.good || '0');
+  const h100 = parseInt(hits?.[100] || hits.ok || '0');
+  const h50 = parseInt(hits?.[50] || hits.meh || '0');
+  const h0 = parseInt(hits?.[0] || hits.miss || '0');
+
+  let accuracy = 0.0;
+  let fc_accuracy = 0.0;
+
+
+  switch (mode) {
+    case GamemodeEnum.osu:
+      accuracy = (100.0 * (6 * h300 + 2 * h100 + h50)) / (6 * (h50 + h100 + h300 + h0));
+
+      fc_accuracy = (100.0 * (6 * (h300 + h0) + 2 * h100 + h50)) / (6 * (h50 + h100 + (h300 + h0) + 0));
+      break;
+
+    case GamemodeEnum.taiko:
+      accuracy = (100.0 * (2 * h300 + h100)) / (2 * (h300 + h100 + h0));
+
+      fc_accuracy = (100.0 * (2 * (h300 + h0) + h100)) / (2 * ((h300 + h0) + h100 + 0));
+      break;
+
+    case GamemodeEnum.fruits:
+      accuracy = (100.0 * (h300 + h100 + h50)) / (h300 + h100 + h50 + katu + h0);
+
+      fc_accuracy = (100.0 * ((h300 + h0) + h100 + h50)) / ((h300 + h0) + h100 + h50 + katu + 0);
+      break;
+
+    case GamemodeEnum.mania:
+      accuracy = (100.0 * (6 * geki + 6 * h300 + 4 * katu + 2 * h100 + h50)) / (6 * (h50 + h100 + h300 + h0 + geki + katu));
+
+      // IT'S NOT CORRECT PLEASE SOMEONE FIX IT
+      fc_accuracy = (100.0 * (6 * (geki + h0) + 6 * h300 + 4 * katu + 2 * h100 + h50)) / (6 * (h50 + h100 + h300 + 0 + (geki + h0) + katu));
+      break;
+
+    default:
+      return { error: new Error(`Unsupported gamemode: ${mode}}`) };
+  };
+
+
+  return {
+    accuracy,
+    fc_accuracy,
+  };
+};
