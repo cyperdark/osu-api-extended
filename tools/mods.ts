54a9fccf807fbeb846a84921b3b33f47fa3a3dcb
ck <21735205+cyperdark@users.noreply.github.com>
2024-04-27 11:59:20 +0300

    testing modsToName speed

--- tools/mods.ts
+++ tools/mods.ts
@@ -4,7 +4,10 @@ import { ModsCodes, ModsOrder } from "../types/enums";
 
 
 
-export const ModsToName = (modsNumber: number): string => {
+export const ModsToName = (modsNumber: number, order?: boolean): string => {
+  if (modsNumber == 0) return '';
+
+
   const convertedParts = [];
   const values = Object.keys(ModsCodes);
   let mods_id = modsNumber;
@@ -20,7 +23,9 @@ export const ModsToName = (modsNumber: number): string => {
   };
 
 
-  convertedParts.sort((a, b) => ModsOrder[a.toLowerCase()] - ModsOrder[b.toLowerCase()]);
+  if (order) {
+    convertedParts.sort((a, b) => ModsOrder[a.toLowerCase()] - ModsOrder[b.toLowerCase()]);
+  };
 
 
   const converted = convertedParts.join('');
@@ -28,48 +33,208 @@ export const ModsToName = (modsNumber: number): string => {
 };
 
 
-export const calculate_mods = (ModsName: string | number): { number: number, name: string } | IError => {
-  if (ModsName == null) {
-    return { error: new Error(`Specify mods name (HDDT or 72)`) };
+export const ModsToName1 = (modsNumber: number, order?: boolean): string => {
+  if (modsNumber == 0) return '';
+
+
+  const convertedParts = [];
+
+  // NF
+  if (modsNumber & 1) convertedParts.push('NF');
+  // EZ
+  if (modsNumber & 2) convertedParts.push('EZ');
+  // TD
+  if (modsNumber & 4) convertedParts.push('TD');
+  // HD
+  if (modsNumber & 8) convertedParts.push('HD');
+  // HR
+  if (modsNumber & 16) convertedParts.push('HR');
+  // SD
+  if (modsNumber & 32) convertedParts.push('SD');
+  // DT
+  if (modsNumber & 64) convertedParts.push('DT');
+  // RX
+  if (modsNumber & 128) convertedParts.push('RX');
+  // HT
+  if (modsNumber & 256) convertedParts.push('HT');
+  // NC
+  if (modsNumber & 576) convertedParts.push('NC');
+  // FL
+  if (modsNumber & 1024) convertedParts.push('FL');
+  // AT
+  if (modsNumber & 2048) convertedParts.push('AT');
+  // SO
+  if (modsNumber & 4096) convertedParts.push('SO');
+  // AP
+  if (modsNumber & 8192) convertedParts.push('AP');
+  // PF
+  if (modsNumber & 16416) convertedParts.push('PF');
+  // 4K
+  if (modsNumber & 32768) convertedParts.push('4K');
+  // 5K
+  if (modsNumber & 65536) convertedParts.push('5K');
+  // 6K
+  if (modsNumber & 131072) convertedParts.push('6K');
+  // 7K
+  if (modsNumber & 262144) convertedParts.push('7K');
+  // 8K
+  if (modsNumber & 524288) convertedParts.push('8K');
+  // FI
+  if (modsNumber & 1048576) convertedParts.push('FI');
+  // RD
+  if (modsNumber & 2097152) convertedParts.push('RD');
+  // LM
+  if (modsNumber & 4194304) convertedParts.push('LM');
+  // Target
+  if (modsNumber & 8388608) convertedParts.push('Target');
+  // 9K
+  if (modsNumber & 16777216) convertedParts.push('9K');
+  // KeyCoop
+  if (modsNumber & 33554432) convertedParts.push('KeyCoop');
+  // 1K
+  if (modsNumber & 67108864) convertedParts.push('1K');
+  // 3K
+  if (modsNumber & 134217728) convertedParts.push('3K');
+  // 2K
+  if (modsNumber & 268435456) convertedParts.push('2K');
+  // ScoreV2
+  if (modsNumber & 536870912) convertedParts.push('ScoreV2');
+  // MR
+  if (modsNumber & 1073741824) convertedParts.push('MR');
+
+
+  if (order) {
+    convertedParts.sort((a, b) => ModsOrder[a.toLowerCase()] - ModsOrder[b.toLowerCase()]);
   };
 
 
-  if (typeof ModsName == 'number') {
-    const name = ModsToName(ModsName);
-    return {
-      number: ModsName,
-      name: name,
+  const converted = convertedParts.join('');
+  return converted;
+};
+
+
+export const ModsToName2 = (modsNumber: number, order?: boolean): string => {
+  const convertedParts = [];
+  let bit = 1;
+
+
+  while (bit <= modsNumber) {
+    switch (bit) {
+      case 1:
+        if (modsNumber & bit) convertedParts.push('NF');
+        break;
+      case 2:
+        if (modsNumber & bit) convertedParts.push('EZ');
+        break;
+      case 4:
+        if (modsNumber & bit) convertedParts.push('TD');
+        break;
+      case 8:
+        if (modsNumber & bit) convertedParts.push('HD');
+        break;
+      case 16:
+        if (modsNumber & bit) convertedParts.push('HR');
+        break;
+      case 32:
+        if (modsNumber & bit) convertedParts.push('SD');
+        break;
+      case 64:
+        if (modsNumber & bit) convertedParts.push('DT');
+        break;
+      case 128:
+        if (modsNumber & bit) convertedParts.push('RX');
+        break;
+      case 256:
+        if (modsNumber & bit) convertedParts.push('HT');
+        break;
+      case 576:
+        if (modsNumber & bit) convertedParts.push('NC');
+        break;
+      case 1024:
+        if (modsNumber & bit) convertedParts.push('FL');
+        break;
+      case 2048:
+        if (modsNumber & bit) convertedParts.push('AT');
+        break;
+      case 4096:
+        if (modsNumber & bit) convertedParts.push('SO');
+        break;
+      case 8192:
+        if (modsNumber & bit) convertedParts.push('AP');
+        break;
+      case 16416:
+        if (modsNumber & bit) convertedParts.push('PF');
+        break;
+      case 32768:
+        if (modsNumber & bit) convertedParts.push('4K');
+        break;
+      case 65536:
+        if (modsNumber & bit) convertedParts.push('5K');
+        break;
+      case 131072:
+        if (modsNumber & bit) convertedParts.push('6K');
+        break;
+      case 262144:
+        if (modsNumber & bit) convertedParts.push('7K');
+        break;
+      case 524288:
+        if (modsNumber & bit) convertedParts.push('8K');
+        break;
+      case 1048576:
+        if (modsNumber & bit) convertedParts.push('FI');
+        break;
+      case 2097152:
+        if (modsNumber & bit) convertedParts.push('RD');
+        break;
+      case 4194304:
+        if (modsNumber & bit) convertedParts.push('LM');
+        break;
+      case 8388608:
+        if (modsNumber & bit) convertedParts.push('Target');
+        break;
+      case 16777216:
+        if (modsNumber & bit) convertedParts.push('9K');
+        break;
+      case 33554432:
+        if (modsNumber & bit) convertedParts.push('KeyCoop');
+        break;
+      case 67108864:
+        if (modsNumber & bit) convertedParts.push('1K');
+        break;
+      case 134217728:
+        if (modsNumber & bit) convertedParts.push('3K');
+        break;
+      case 268435456:
+        if (modsNumber & bit) convertedParts.push('2K');
+        break;
+      case 536870912:
+        if (modsNumber & bit) convertedParts.push('ScoreV2');
+        break;
+      case 1073741824:
+        if (modsNumber & bit) convertedParts.push('MR');
+        break;
+      default:
+        break;
     };
+
+
+    bit <<= 1;
   };
 
 
-  let mods_id = 0;
-  const ModsArray = ModsName.match(/.{1,2}/g);
-  if (!Array.isArray(ModsArray)) {
-    return { error: new Error(`Can't convert mods (${ModsName}) to array of mods`) };
+  if (order) {
+    convertedParts.sort((a, b) => ModsOrder[a.toLowerCase()] - ModsOrder[b.toLowerCase()]);
   };
 
 
-  const codes = Object.keys(ModsCodes);
-  for (let i = 0; i < ModsArray.length; i++) {
-    const mod_name = ModsArray[i];
-
-    const find = codes.find(v => ModsCodes[v].toLowerCase() === mod_name?.toLowerCase());
-    if (find == null) continue;
-
-    mods_id += parseInt(find);
-  };
-
-
-  return {
-    number: mods_id,
-    name: ModsName,
-  };
+  const converted = convertedParts.join('');
+  return converted;
 };
 
 
 
-export const calculate_mods_switch = (ModsName: string | number): { number: number, name: string } | IError => {
+// SWITCH STATEMENT CREATED ON PURPOSE BECAUSE IT'S WAY FASTER
+export const calculate_mods = (ModsName: string | number): { number: number, name: string } | IError => {
   if (ModsName == null) {
     return { error: new Error(`Specify mods name (HDDT or 72)`) };
   };
@@ -190,124 +355,6 @@ export const calculate_mods_switch = (ModsName: string | number): { number: numb
         break;
     };
   };
-
-
-  return {
-    number: mods_id,
-    name: ModsName,
-  };
-};
-
-
-export const calculate_mods_if = (ModsName: string | number): { number: number, name: string } | IError => {
-  if (ModsName == null) {
-    return { error: new Error(`Specify mods name (HDDT or 72)`) };
-  }
-
-  if (typeof ModsName === 'number') {
-    return {
-      number: ModsName,
-      name: ModsToName(ModsName),
-    };
-  }
-
-
-  let mods_id = 0;
-  let mods_name = ModsName.toLowerCase();
-
-  if (mods_name.includes('nf')) {
-    mods_id += 1;
-  }
-  if (mods_name.includes('ez')) {
-    mods_id += 2;
-  }
-  if (mods_name.includes('td')) {
-    mods_id += 4;
-  }
-  if (mods_name.includes('hd')) {
-    mods_id += 8;
-  }
-  if (mods_name.includes('hr')) {
-    mods_id += 16;
-  }
-  if (mods_name.includes('sd')) {
-    mods_id += 32;
-  }
-  if (mods_name.includes('dt')) {
-    mods_id += 64;
-  }
-  if (mods_name.includes('rx')) {
-    mods_id += 128;
-  }
-  if (mods_name.includes('ht')) {
-    mods_id += 256;
-  }
-  if (mods_name.includes('nc')) {
-    mods_id += 576;
-  }
-  if (mods_name.includes('fl')) {
-    mods_id += 1024;
-  }
-  if (mods_name.includes('at')) {
-    mods_id += 2048;
-  }
-  if (mods_name.includes('so')) {
-    mods_id += 4096;
-  }
-  if (mods_name.includes('ap')) {
-    mods_id += 8192;
-  }
-  if (mods_name.includes('pf')) {
-    mods_id += 16416;
-  }
-  if (mods_name.includes('4k')) {
-    mods_id += 32768;
-  }
-  if (mods_name.includes('5k')) {
-    mods_id += 65536;
-  }
-  if (mods_name.includes('6k')) {
-    mods_id += 131072;
-  }
-  if (mods_name.includes('7k')) {
-    mods_id += 262144;
-  }
-  if (mods_name.includes('8k')) {
-    mods_id += 524288;
-  }
-  if (mods_name.includes('fi')) {
-    mods_id += 1048576;
-  }
-  if (mods_name.includes('rd')) {
-    mods_id += 2097152;
-  }
-  if (mods_name.includes('lm')) {
-    mods_id += 4194304;
-  }
-  if (mods_name.includes('target')) {
-    mods_id += 8388608;
-  }
-  if (mods_name.includes('9k')) {
-    mods_id += 16777216;
-  }
-  if (mods_name.includes('keycoop')) {
-    mods_id += 33554432;
-  }
-  if (mods_name.includes('1k')) {
-    mods_id += 67108864;
-  }
-  if (mods_name.includes('3k')) {
-    mods_id += 134217728;
-  }
-  if (mods_name.includes('2k')) {
-    mods_id += 268435456;
-  }
-  if (mods_name.includes('scorev2')) {
-    mods_id += 536870912;
-  }
-  if (mods_name.includes('mr')) {
-    mods_id += 1073741824;
-  }
 
 
   return {
