123e08060d1410ed01acca7f86d156b5bdf62ff9
ck <21735205+cyperdark@users.noreply.github.com>
2024-05-14 19:20:28 +0300

    net_pp: make it universal + fixes

--- tools/net-pp.ts
+++ tools/net-pp.ts
@@ -1,45 +1,61 @@
-import { v2 } from "../";
-import type { Modes_names } from "../types";
+import type { IError, Modes_names } from "../types";
+import { handleErrors } from "../utility/handleErrors";
 
-export const calculate_net_pp = async (params: {
-    user_id: number;
-    pp_value: number;
-    gamemode?: Modes_names;
-}): Promise<number> => {
-    // fetch user's top plays
-    const top_plays = await v2.scores.list({
-        type: "user_best",
-        user_id: params.user_id,
-        limit: 100,
-    });
+import { ScoresListUserBestResponse } from "../types/v2/scores_list_user_best";
 
-    // map returned data to only pp values.
-    const old_scores = top_plays.map(x => x.pp);
 
-    // create a copy of the top plays array for simulation.
-    const new_scores = [...top_plays];
+type Response = {
+  pp: number;
+  totalNow: number;
+  totalBefore: number;
+} & IError;
 
-    // return 0.0 if the passed pp value is lower than the lowest best score.
-    if (params.pp_value < Math.min(...old_scores)) return 0.0;
 
-    // weight the user's current scores. (see https://osu.ppy.sh/wiki/en/Performance_points/Weighting_system)
-    const weighted_old_scores = old_scores.map((x, i) => x * Math.pow(0.95, i));
+export const calculate_net_pp = (params: {
+  scores: ScoresListUserBestResponse[] | number[];
+  pp_value: number;
+  gamemode?: Modes_names;
+}): Response => {
+  if (!Array.isArray(params.scores)) {
+    return handleErrors(`Provide array of scores or numbers`) as Response;
+  };
 
-    // change the last score to the new value.
-    new_scores[new_scores.length - 1].pp = params.pp_value;
+  if (!isFinite(params.pp_value) || params.pp_value == null) {
+    return handleErrors(`Specify pp`) as Response;
+  };
 
-    // sort the array by pp value (descending).
-    new_scores.sort((a, b) => b.pp - a.pp);
 
-    // weight the new scores. (see https://osu.ppy.sh/wiki/en/Performance_points/Weighting_system)
-    const weighted_new_scores = new_scores.map((x, i) => x.pp * Math.pow(0.95, i));
+  const pp_values = typeof params.scores[0] == 'number' ? params.scores : (params.scores as any[]).map(r => r.pp);
+  pp_values.sort((a, b) => b - a);
 
-    // sum up the total pp value of the user's current scores.
-    const total_pp_old = weighted_old_scores.reduce((a, b) => a + b, 0);
+  // weight the user's current scores. (see https://osu.ppy.sh/wiki/en/Performance_points/Weighting_system)
+  const weighted_before = pp_values.slice(0, 100).map((x, i) => x * Math.pow(0.95, i));
+  // sum up the total pp value of the user's current scores.
+  const total_pp_old = weighted_before.reduce((a, b) => a + b, 0);
 
-    // sum up the total pp value of the user's new simulated scores.
-    const total_pp_new = weighted_new_scores.reduce((a, b) => a + b, 0);
 
-    // return the delta between the two pp values.
-    return total_pp_new - total_pp_old;
+  if (params.pp_value < Math.min(pp_values[pp_values.length - 1]))
+    return {
+      pp: 0,
+      totalNow: total_pp_old,
+      totalBefore: total_pp_old,
+    } as Response;
+
+
+  // Push new pp_value
+  pp_values.push(params.pp_value);
+  pp_values.sort((a, b) => b - a);
+
+
+  // weight the new scores. (see https://osu.ppy.sh/wiki/en/Performance_points/Weighting_system)
+  const weighted_after = pp_values.slice(0, 100).map((x, i) => x * Math.pow(0.95, i));
+  // sum up the total pp value of the user's new simulated scores.
+  const total_pp_new = weighted_after.reduce((a, b) => a + b, 0);
+
+
+  return {
+    pp: total_pp_new - total_pp_old,
+    totalNow: total_pp_new,
+    totalBefore: total_pp_old,
+  } as Response;
 };
