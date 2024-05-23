22ac034f3f05744fc1dc5a9d167354c102150796
GabuTheDev <107962621+GabuTheDev@users.noreply.github.com>
2024-05-23 22:53:19 +0300

    Add `addons` param to `v2/chat_channels_list.ts`.

--- api/v2/chat_channels_list.ts
+++ api/v2/chat_channels_list.ts
@@ -1,11 +1,11 @@
-import { IError } from "../../types";
+import {IDefaultParams, IError} from "../../types";
 import { chatChannelsListResponse } from "../../types/v2/chat_channels_list";
 import { handleErrors } from "../../utility/handleErrors";
 import { request } from "../../utility/request";
 
 
 
-export const chat_channels_list = async (): Promise<chatChannelsListResponse[] & IError> => {
+export const chat_channels_list = async (addons?: IDefaultParams): Promise<chatChannelsListResponse[] & IError> => {
   const data = await request(`https://osu.ppy.sh/api/v2/chat/channels`, {
     method: 'GET',
   });
