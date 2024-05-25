7c99b36797a2209b1a8d194122bc40e208d96762
ck <2-ck@users.noreply.git.lab>
2024-05-26 02:27:21 +0300

    typo fix

--- api/v2/notifications_list.ts
+++ api/v2/notifications_list.ts
@@ -9,7 +9,7 @@ type Response = NotificationsListResponse & IError;
 
 export const notifications_list = async (params: {
   max_id: string;
-  unreaded_only: boolean;
+  unread_only: boolean;
 }, addons?: IDefaultParams): Promise<Response> => {
   const data = await request(`https://osu.ppy.sh/api/v2/notifications`, {
     method: 'GET',
@@ -21,7 +21,7 @@ export const notifications_list = async (params: {
   if (data.error) return handleErrors(data.error);
 
 
-  if (params.unreaded_only == true && !('error' in data))
+  if (params.unread_only == true && !('error' in data))
     data.notifications = data.notifications.filter((r: any) => r.is_read == false);
 
   return data;
