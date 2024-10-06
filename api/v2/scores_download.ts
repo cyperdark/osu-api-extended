8f898f52aa71baa7d69360600a40d74bd1053014
ck <21735205+cyperdark@users.noreply.github.com>
2024-10-06 07:13:59 +0300

    prevent download until path is specified

--- api/v2/scores_download.ts
+++ api/v2/scores_download.ts
@@ -11,7 +11,7 @@ type Response = ScoresDownloadResponse & IError;
 export const scores_download = async (params: {
   id: number;
   mode?: Modes_names;
-  file_path?: string;
+  file_path: string;
 }): Promise<Response> => {
   if (credentials.type != 'lazer' && credentials.type != 'cli') {
     return handleErrors(new Error(`Login via lazer or cli to use this endpoint`)) as Response;
@@ -19,6 +19,10 @@ export const scores_download = async (params: {
 
   if (params?.id == null) {
     return handleErrors(new Error(`Specify score id`)) as Response;
+  };
+
+  if (params?.file_path == null) {
+    return handleErrors(new Error(`Specify file_path`)) as Response;
   };
 
 
