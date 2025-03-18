234103d3a6a4f01d783837cf373ca1f4b88bf3a6
ck <21735205+cyperdark@users.noreply.github.com>
2025-03-19 02:46:18 +0300

    prepare for future

--- api/v2/beatmaps_actions.ts
+++ api/v2/beatmaps_actions.ts
@@ -1,4 +1,4 @@
-// import { request } from "../../utility/request";
+import { request } from "../../utility/request";
 import { IDefaultParams, IError } from "../../types";
 import { handleErrors } from "../../utility/handleErrors";
 // import { postDataObject } from "../../tools";
@@ -7,25 +7,38 @@ import { handleErrors } from "../../utility/handleErrors";
 type params = ({
   type: 'favourite';
 
-  beatmapset_id: number;
-  add: boolean;
+  /** beatmap set id */
+  id: number;
+  status: boolean;
+} | {
+  type: 'tag';
+
+  /** beatmap id */
+  id: number;
+  /** tag name */
+  name: string;
+  status: boolean;
 });
 
 
 type Response<T extends params['type']> =
-  T extends 'difficulty'
-  ? any & IError
-  : T extends 'difficulties'
-  ? any & IError
+  T extends 'favourite'
+  ? string & IError
+  : T extends 'tag'
+  ? string & IError
   : IError;
 
-// FIX
-const name = async <T extends params>(params: T, addons?: IDefaultParams): Promise<Response<T['type']>> => {
-  return handleErrors(new Error('TEMPORARY NOT WORKING, have no clue why')) as Response<T['type']>;
-  // if (params.type == null)
-  //   return {
-  //     error: 'Type not specified',
-  //   };
+
+export const beatmaps_actions = async <T extends params>(params: T, addons?: IDefaultParams): Promise<Response<T['type']>> => {
+  if (params.type == 'favourite' || params.type == 'tag') {
+    return handleErrors(new Error(`currently osu! api does not support this action`)) as Response<T['type']>;
+  };
+
+  // if (params.id == null) {
+  //   if (params.type == 'favourite') return handleErrors(new Error(`Specify beatmapset id`)) as Response<T['type']>;
+  //   return handleErrors(new Error(`Specify beatmap id`)) as Response<T['type']>;
+  // };
+
 
   // const object: any = {};
   // let url = 'https://osu.ppy.sh/api/v2';
@@ -33,24 +46,32 @@ const name = async <T extends params>(params: T, addons?: IDefaultParams): Promi
 
 
   // switch (params.type) {
-  //   case 'favourite':
-  //     url += `/beatmapsets/${params.beatmapset_id}/favourites`;
-
-  //     object['action'] = params.add == true ? 'favourite' : 'unfavourite';
+  //   case 'favourite': {
+  //     url += `/beatmapsets/${params.id}/favourites`;
+  //     object['action'] = params.status == true ? 'favourite' : 'unfavourite';
   //     break;
+  //   };
+
+  //   case 'tag': {
+  //     if (params.name == null || params.name == '') return handleErrors(new Error(`Specify tag name`)) as Response<T['type']>;
+
+  //     url += `/beatmaps/${params.id}/tags/${params.name}`;
+  //     method = params.status == true ? 'PUT' : 'DELETE';
+  //     break;
+  //   }
+
+  //   default:
+  //     return handleErrors(new Error(`Unsupported type: ${(params as any).type}`)) as Response<T['type']>;
   // };
 
 
   // const data = await request(url, {
   //   method: method,
-  //   // params: object,
-  //   data: postDataObject('', object),
+  //   params: object,
   //   addons,
   // });
+  // if (data.error) return handleErrors(new Error(data.error)) as Response<T['type']>;
 
 
   // return data as Response<T['type']>;
 };
-
-
-export default name;
